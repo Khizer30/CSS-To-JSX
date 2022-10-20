b52a7c8aa7893a3b4b7100e7169e9fd198e06371
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2022-10-21 00:12:44 +0500

    v1.00
    *Development Complete

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,92 @@
 <script lang="ts">
   // ...
   import "./styles/styles.css" ;
+
+  // Variables
+  let css: string = "" ;
+  let jsx: string = "" ;
+  let words: string[] = [] ;
+  let selected: string[] = [] ;
+
+  // Reactive
+  $:
+  {
+    words = css.trim().split(" ") ;
+    selected = [] ;
+
+    createJSX() ;
+  }
+
+  // Select
+  function select(word: string): void
+  {
+    let index: number = selected.indexOf(word) ;
+
+    if (index === -1)
+    {
+      selected.push(word) ;
+    }
+    else
+    {
+      selected.splice(index, 1) ;
+    }
+
+    createJSX() ;
+
+    // Render
+    selected = selected ;
+  }
+
+  // Create JSX
+  function createJSX(): void
+  {
+    let temp: string = css.trim() ;
+
+    // Remove Selected
+    for (let i: number = 0; i < selected.length; i++)
+    {
+      temp = temp.replaceAll(`${ selected[i] } `, "") ;
+      temp = temp.replaceAll(` ${ selected[i] }`, "") ;
+      temp = temp.replaceAll(selected[i], "") ;
+    }
+
+    // Add Selected
+    let newTemp: string = temp ? `className={ "${ temp } "` : `className={ ` ;
+
+    if (selected.length === 0)
+    {
+      newTemp = `className={ "${ temp }"` ;
+    }
+
+    for (let i: number = 0; i < selected.length; i++)
+    {
+      newTemp += ` + styles.${ selected[i] }` ;
+    }
+
+    if (selected.length === words.length)
+    {
+      newTemp = newTemp.replace(" + ", "") ;
+    }
+
+    newTemp += ` }` ;
+
+    // Render
+    if (newTemp !== `className={ "" }`)
+    {
+      jsx = newTemp ;
+    }
+    else
+    {
+      jsx = "" ;
+    }
+  }
+
+  // Copy
+  function copy(): void
+  {
+    navigator.clipboard.writeText(jsx) ;
+    alert("JSX Copied!") ;
+  }
 </script>
 
 <nav class="navbar navbar-light navbar-expand-md">
@@ -13,39 +99,51 @@
   <form method="post" target="_self" class="width80"
   enctype="application/x-www-form-urlencoded" autocomplete="off" novalidate>
 
-    <p class="error"> Error </p>
+    <p class="error"> <br /> </p>
 
     <input
-      name="names"
+      name="css"
       type="text"
-      value=""
+      bind:value={ css }
       maxlength="500"
       minlength="0"
-      placeholder="CSS"
+      placeholder="Enter CSS Here..."
       required
       autofocus
       class="form-control textInput"
     />
 
     <div class="d-flex justify-content-evenly align-items-center">
-      <button type="button" class="mainBtn"> mainDiv </button>
+    { #each words as word }
+      { #if (word !== "") }
+        <button on:click={ () => select(word) } type="button" class="mainBtn"> { word } </button>
+      { :else }
+        <button type="button" class="mainBtn hidden"> NULL </button>
+      { /if }
+    { /each }
     </div>
     
     <input
-      name="names"
+      name="jsx"
       type="text"
-      value=""
+      bind:value={ jsx }
       maxlength="500"
       minlength="0"
-      placeholder="JSX"
+      placeholder="Your JSX Will Appear Here..."
       required
       class="form-control textInput"
     />
 
     <div class="d-flex justify-content-evenly align-items-center">
-      <button type="button" class="d-flex justify-content-center align-items-center copyBtn">
+    { #if jsx }
+      <button on:click={ copy } type="button" class="d-flex justify-content-center align-items-center copyBtn">
         <i class="far fa-copy"></i>
       </button>
+    { :else }
+      <button type="button" class="d-flex justify-content-center align-items-center copyBtn hidden">
+        <i class="far fa-copy"></i>
+      </button>
+    { /if }
     </div>
 
   </form>
